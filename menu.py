8539fe48bac4ebc6f898e55a1a090b72018c5ee1
Jed Smith <jedsmith@pm.me>
2021-12-12 16:25:52 -0500

    scrap exception because printing the error didn't work

--- menu.py
+++ menu.py
@@ -2,21 +2,16 @@ import nuke
 import sys
 
 
-# Import python tools
-# This setup automatically imports all python files in the specified folders.
-# Check the code in the __init__.py file inside each folder for how this is done.
+''' Import python tools
+    This setup automatically imports all python files in the specified folders.
+    Check the code in the __init__.py file inside each folder for how this is done.
+'''
 
-try:
-    # Tools written by jed
-    from nuketools import *
+# Tools written by jed
+from nuketools import *
 
-    # Useful tools brought in from Nukepedia, mostly unchanged
-    from nukepedia import *
+# Useful tools brought in from Nukepedia, mostly unchanged
+from nukepedia import *
 
-    # Set up drop handler (not implemented yet)
-    # drop.registerDropCallback()
-
-except Exception as exc:
-    # If there is an exception loading one of the tools
-    # it will be handled and the error message will print
-    sys.stderr.write('A problem loading tools occured...\n\n{0}'.format(exc))
+# Set up drop handler (not implemented yet)
+# drop.registerDropCallback()
