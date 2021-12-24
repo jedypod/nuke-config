a5227745197be8197a097e0c47f4a35efb9f8d34
Jed Smith <jedsmith@pm.me>
2021-12-23 22:37:05 -0500

    rerganize again.

--- init.py
+++ init.py
@@ -4,20 +4,9 @@ import os, errno
 
 # The folder of this init.py file
 base_path = os.path.dirname(__file__) 
-
-
-# plugin_dirs is a list of folders to add to the $NUKE_PATH plugin path.
-# All subdirectories of each folder will also be added.
-plugin_dirs = ['tools']
-for plugin_dir in plugin_dirs:
-    fullpath = os.path.join(base_path, plugin_dir)
-    nuke.tprint('Loading plugin directory: {0}'.format(fullpath))
-    if os.path.isdir(fullpath):
-        nuke.pluginAddPath(fullpath)
-        subdirs = [d for d in os.listdir(fullpath) if os.path.isdir(os.path.join(fullpath, d))]
-        for subdir in subdirs:
-            nuke.pluginAddPath(os.path.join(fullpath, subdir))
-
+pdirs = ['scripts']
+for pdir in pdirs:
+    nuke.pluginAddPath(os.path.join(base_path, pdir))
 
 
 # Callbacks
--- menu.py
+++ menu.py
@@ -1,17 +1,13 @@
 import nuke
-import sys
 
-
-''' Import python tools
-    This setup automatically imports all python files in the specified folders.
-    Check the code in the __init__.py file inside each folder for how this is done.
-'''
-
-# Tools written by jed
+# Auto-imports all python modules inside nuketools folder
+# Check the code in the __init__.py file for how this is done.
 from nuketools import *
 
-# Useful tools brought in from Nukepedia, mostly unchanged
-from nukepedia import *
 
-# Set up drop handler (not implemented yet)
+# Shortcut Editor gets imported last so it can override everything else.
+import shortcuteditor
+shortcuteditor.nuke_setup()
+
+# Set up drop handler
 # drop.registerDropCallback()
