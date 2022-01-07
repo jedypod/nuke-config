c50131b70700958e3d9dc192e5e104c3682aff20
Jed Smith <jedsmith@pm.me>
2022-01-07 18:00:29 -0500

    tweaks

--- scripts/nuketools/shortcuts.py
+++ scripts/nuketools/shortcuts.py
@@ -32,23 +32,29 @@ nuke.addOnCreate(hide_status_bar, nodeClass='Root')
 
 
 
+nuke.menu('Viewer').findItem('Show Overscan').setShortcut('alt+shift+o')
+
+
+
 # Nodes
 nuke.menu('Nodes').addCommand('Merge/Merge', 'nuke.createNode("Merge2", "bbox B", inpanel=False)', 'm', shortcutContext=2)
-nuke.menu('Nodes').addCommand('Merge/Merges/MergeUnion', 'nuke.createNode("Merge2", "bbox union", inpanel=False)', 'alt+meta+m', shortcutContext=2)
+nuke.menu('Nodes').addCommand('Merge/Merges/MergeUnion', 'nuke.createNode("Merge2", "bbox union", inpanel=False)', 'meta+m', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Merges/Plus', 'nuke.createNode("Merge2", "operation plus output rgb", inpanel=False)', 'alt+meta+p', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Merges/From', 'nuke.createNode("Merge2", "operation from output rgb", inpanel=False)', 'alt+meta+f', shortcutContext=2)
+nuke.menu('Nodes').addCommand('Merge/Merges/Minus', 'nuke.createNode("Merge2", "operation minus output rgb", inpanel=False)', 'alt+meta+n', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Merges/Mask', 'nuke.createNode("Merge2", "operation mask bbox intersection", inpanel=False)', 'alt+meta+a', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Merges/Stencil', 'nuke.createNode("Merge2", "operation stencil bbox B", inpanel=False)', 'alt+meta+s', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Merges/Under', 'nuke.createNode("Merge2", "operation under bbox B", inpanel=False)', 'alt+meta+u', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Merges/Divide', 'nuke.createNode("Merge2", "operation divide", inpanel=False)', 'alt+meta+d', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Merges/Multiply', 'nuke.createNode("Merge2", "operation multiply output rgb", inpanel=False)', 'alt+meta+t', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Merges/MultiplyAlpha', 'nuke.createNode("Merge2", "operation multiply", inpanel=False)', 'alt+shift+meta+t', shortcutContext=2)
-nuke.menu('Nodes').addCommand('Merge/Merges/MergeAlpha', 'nuke.createNode("Merge2", "operation screen bbox union Achannels alpha Bchannels alpha output alpha", inpanel=False)', 'alt+meta+shift+e', shortcutContext=2)
-nuke.menu('Nodes').addCommand('Merge/Merges/Screen', 'nuke.createNode("Merge2", "operation screen bbox union", inpanel=False)', 'alt+meta+e', shortcutContext=2)
+nuke.menu('Nodes').addCommand('Merge/Merges/MergeAlpha', 'nuke.createNode("Merge2", "operation screen bbox union Achannels alpha Bchannels alpha output alpha", inpanel=False)', 'alt+meta+shift+n', shortcutContext=2)
+nuke.menu('Nodes').addCommand('Merge/Merges/Screen', 'nuke.createNode("Merge2", "operation screen bbox union", inpanel=False)', 'alt+meta+n', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Premult', 'nuke.createNode("Premult", inpanel=False)', '\\', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Keymix', 'nuke.createNode("Keymix", "channels rgba", inpanel=False)', 'alt+meta+k', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Merge/Unpremult', 'nuke.createNode("Unpremult", inpanel=False)', 'alt+\\', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Channel/ChannelMerge', 'nuke.createNode("ChannelMerge", "operation union", inpanel=False)', 'alt+meta+c', shortcutContext=2)
+nuke.menu('Nodes').addCommand('Filter/Blur', "nuke.createNode('Blur')", 'b', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Color/ColorLookup', 'nuke.createNode("ColorLookup")', 'meta+alt+shift+c', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Channel/Shuffle_', 'nuke.createNode("Shuffle")', 'h', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Channel/ShuffleCopy_', 'nuke.createNode("ShuffleCopy", "label [value\ in]\ |\ [value\ in2]\ ->\ [value\ out]")', 'alt+meta+h', shortcutContext=2)
@@ -59,7 +65,6 @@ nuke.menu('Nodes').addCommand('Color/GradeAlpha', 'nuke.createNode("Grade", "nam
 nuke.menu('Nodes').addCommand('Color/HueCorrect', 'nuke.createNode("HueCorrect", "")', 'alt+shift+h', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Color/Math/Multiply', 'nuke.createNode("Multiply", "channels rgb")', 'alt+m', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Color/Math/MultiplyAlpha', 'nuke.createNode("Multiply", "channels rgba")', 'alt+shift+m', shortcutContext=2)
-nuke.menu('Nodes').addCommand('Filter/Blur', "nuke.createNode('Blur')", 'b', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Filter/FilterErode', 'nuke.createNode("FilterErode")', 'alt+meta+shift+e', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Filter/Dilate', 'nuke.createNode("Dilate")', 'alt+shift+e', shortcutContext=2)
 nuke.menu('Nodes').addCommand('Time/FrameHold', 'nuke.createNode("FrameHold", "first_frame {0}".format(nuke.frame()), inpanel=False)', 'meta+alt+shift+f', icon="FrameHold.png", shortcutContext=2)
@@ -72,3 +77,7 @@ nuke.menu('Nodes').addCommand('Draw/Bezier', 'nuke.createNode("Bezier")', 'meta+
 # nuke.menu('Nodes').addCommand('Color/Exposure', lambda: nuke.nodePaste(os.path.expanduser("~/.nuke/ToolSets/Color/Exposure.nk")), 'meta+e', shortcutContext=2)
 # nuke.menu('Nodes').addCommand('Color/Mult', lambda: nuke.nodePaste(os.path.expanduser("~/.nuke/ToolSets/Color/Mult.nk")), 'alt+meta+g', shortcutContext=2)
 # nuke.menu('Nodes').addCommand('Filter/BlurPercent', lambda: nuke.nodePaste(os.path.expanduser("~/.nuke/ToolSets/Filter/BlurPercent.nk")), 'alt+meta+b', shortcutContext=2)
+
+
+# Re-assign rename shortcut
+# nuke.menu('Nuke').addCommand('Edit/Node/Rename', '', 'alt+shift+k', shortcutContext=2)
